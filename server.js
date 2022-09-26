f0aecef9ca0af36e7340341f139dce1bd6d77018
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-09-26 14:06:38 +0200

    Respond to POST // "Sort of"

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-   // set something so that GET /login returns user name
+   // set something so that GET /login returns username
 
-
+   res.end();
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
