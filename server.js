232192966468d0694386d1146775c35737543f28
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-09-26 14:18:57 +0200

    Respond to POST /login

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
+
+const USERS = [
+   {
+      username: 'admin',
+      password: '1234'
+   }
+];
 
 app.get('/login', (req, res) => {
    res.json({
@@ -8,10 +17,23 @@ app.get('/login', (req, res) => {
    });
 });
 
+/**
+ * Type: POST
+ * Route: /login
+ * Parameters: username and password
+ * Description: Logs user into the system
+ * */
 app.post('/login', (req, res) => {
-   // set something so that GET /login returns username
+   const body = req.body;
+   const { username, password } = body;
 
-   res.end();
+   const user = USERS.find(x => x.username === username && x.password == password);
+
+   if(user) {
+      res.sendStatus(200);
+   } else {
+      res.sendStatus(401);
+   }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
