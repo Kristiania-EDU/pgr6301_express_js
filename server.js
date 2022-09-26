c28ea2d7c5cc67a783c2c6c0a9e066a86fe26216
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-09-26 13:57:31 +0200

    Server listening to port 3000 and GET-endpoint at /login

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 import express from 'express';
 
 const app = express();
+
+app.get('/login', (req, res) => {
+   res.send('You reaced /login');
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+   const address = server.address();
+
+   console.log(`Server started at http://localhost:${address.port}`);
+});
